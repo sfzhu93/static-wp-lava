5c029e182f89d5d642aea8d7d73775d717c012b9
Shuofei Zhu <zhushuofei@gmail.com>
2019-03-27 02:00:09 -0400

    WpPrinter

---
+++ src/WpPrinter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zsf on 19-3-27.
+//
+
+#include "WpPrinter.h"
---
+++ src/WpPrinter.h
@@ -0,0 +1,14 @@
+//
+// Created by zsf on 19-3-27.
+//
+
+#ifndef STATIC_WP_LAVA_WPPRINTER_H
+#define STATIC_WP_LAVA_WPPRINTER_H
+
+
+class WpPrinter {
+
+};
+
+
+#endif //STATIC_WP_LAVA_WPPRINTER_H
